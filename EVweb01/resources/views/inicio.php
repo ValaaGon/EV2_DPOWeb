576827950e278a1cfe6facabfd07c8569d4ab032
ValaaGon <vale.gon1218@gmail.com>
2023-05-24 22:42:04 -0600

    Actualizacion

---
+++ EVweb01/resources/views/inicio.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+      crossorigin="anonymous"
+    ></script>
+
+    <title>Home</title>
+  </head>
+
+  
+
+    <!-- navbar -->
+    <div class="container-fluid px-0">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">EV2_DPOWeb</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"
+                  >Inicio</a
+                >
+              </li>
+             
+
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Estudiante
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark bg-primary">
+                  <li>
+                    <a class="dropdown-item" href="#">Ingresar propuesta</a>
+                  </li>
+                  <li><a class="dropdown-item" href="#">Estado de revision</a></li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Retroalimentacion</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Administrador
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark bg-primary">
+                  <li>
+                    <a class="dropdown-item" href="#">Gestion de profesores</a>
+                  </li>
+                  <li><a class="dropdown-item" href="#">Gestion de alumnos</a></li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Propuestas</a>
+                  </li>
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Profesor
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark bg-primary">
+                  <li>
+                    <a class="dropdown-item" href="#">Propuestas</a>
+                  </li>
+                 
+
+              <li class="nav-item d-lg-none">
+                <a class="nav-link" href="">Cerrar Sesión</a>
+              </li>
+            </ul>
+
+            
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    
+         
+        
+
+    
+  </body>
+</html>
